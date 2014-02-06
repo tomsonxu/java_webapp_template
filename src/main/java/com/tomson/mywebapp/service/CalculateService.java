3392d73909bb81c2fd5e691025e5f7c819d89d30
tomsonxu <tomson.tao.xu@gmail.com>
2014-02-06 15:37:30 +0800

    Add log feature with slf4j and logback lib 1. add slf4j and logback dependency to pom.xml 2. add logback xml configuration files: src/main/resources/logback.xml and src/test/resources/logback-test.xml 3. add some scripts about log in com.tomson.mywebapp.service.CalculateService class

--- src/main/java/com/tomson/mywebapp/service/CalculateService.java
+++ src/main/java/com/tomson/mywebapp/service/CalculateService.java
@@ -1,7 +1,16 @@
 package com.tomson.mywebapp.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CalculateService {
+	private final Logger logger = LoggerFactory.getLogger(CalculateService.class);
+	
 	public int add(int a, int b){
+		//for logback demo
+		logger.info("call add method [info level]");		
+		logger.debug("call add method [debug level]");	
+		
 		return a + b;
 	}
 }
